158079acd066d2241ff38325c37c6e74b2d2cbba
chenjieGu <xytxjt@gmail.com>
2015-04-11 09:42:35 +0800

    add compare function  number_compare

--- src/iippy-3.py
+++ src/iippy-3.py
@@ -4,6 +4,7 @@ import random
 
 #init 
 record_list = []
+record_list_round = [] 
 
 #helper functions
 
@@ -13,31 +14,34 @@ class number_guess:
     # initializer for range
     def __init__(self, range):
         self.range = range
-
     # choose a number in a particular range
     def number_choose(self, number_chosen):
     	self.number_chosen = random.randrange(self.range[0],self.range[1])
-	def number_compare(self,delta):
-		self.delta = self.number_chosen - (self.range[1] - self.range[0])/2.0
 	# method that updates the range computer to guess   
-    def update_range(self):
-    	if self.delta > 0:
-    		self.range[0] = (self.range[1] - self.range[0])/2.0
-    		self.range[1] = self.number_chosen
-		else:
-			self.range[1] = (self.range[1] - self.range[0])/2.0
-    		self.range[0] = self.number_chosen
+    def update_range(self,update):
+    	self.range[0] += update[0]
+        self.range[1] += update[1]
+    
+# algorithm to change range 
+def number_compare(number_pick,number_guess,range):
+	i = 0
+	if number_guess > number_pick:
+		i = 1
+	else:
+		i = 0
+	range[i] = (range[i] + number_guess)/2.0
+	range[int(not i)] = number_guess
 
 
 
-#event handlers
+# event handlers
 
-#create frame
+
+# create frame
 
 # create a record_list
-record_list = []
-for i in range(100):
-	n = number_guess([0, 100])
+
+
 
 #start game
 
