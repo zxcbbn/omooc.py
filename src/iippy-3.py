2bf9c8aa96821e59ad52d46370d70c97c7c65445
chenjieGu <xytxjt@gmail.com>
2015-04-11 11:47:29 +0800

    add game_start

--- src/iippy-3.py
+++ src/iippy-3.py
@@ -5,43 +5,75 @@ import random
 #init 
 record_list = []
 record_list_round = [] 
+count = 0
+range_choose = [0,100]
 
 #helper functions
 
 # definition of number_guess class
 class number_guess:
-    
     # initializer for range
-    def __init__(self, range):
-        self.range = range
+    def __init__(self, range_choose):
+        self.range = range_choose
     # choose a number in a particular range
-    def number_choose(self, number_chosen):
-    	self.number_chosen = random.randrange(self.range[0],self.range[1])
-	# method that updates the range computer to guess   
-    def update_range(self,update):
-    	self.range[0] += update[0]
-        self.range[1] += update[1]
+    def number_choose(self):
+    	self.number_chosen = random.randint(self.range[0],self.range[1])
+    # review 
+    def review(self):
+    	number_chosen = self.number_chosen
+    	global record_list_round
+    	global record_list
+    	global count
+    	record_list_round.append(number_chosen)
+    	if record_list_round[0] != record_list_round[count]:
+    		count = count + 1
+    		record_list.append(record_list_round)
+    		count = 0
+    		record_list_round = [] 
     
+# compare guessed number and picked number
+def number_compare(number_picked,number_guessed):
+    global delta
+    delta = number_guessed - number_picked
+    if delta == 0:
+    	print "Amazing"
+    elif delta > 0:
+    	print "Lower"
+    else:
+    	print "Higher"
 # algorithm to change range 
-def number_compare(number_pick,number_guess,range):
+def range_change(range_choose,number_guessed):
+	global delta
 	i = 0
-	if number_guess > number_pick:
-		i = 1
-	else:
+	if delta < 0:
 		i = 0
-	range[i] = (range[i] + number_guess)/2.0
-	range[int(not i)] = number_guess
-
+	else :
+		i = 1
+	range_choose[i] = (range_choose[i] + number_guessed)/2.0
+	range_choose[int(not i)] = number_guessed
 
 
 # event handlers
+def game_start():
+	global range_choose
+	num_pick = number_guess(range_choose)
+	num_pick.review
+	num_guess = number_guess(range_choose)
+	num_guess.review
+	print num_guess
+	number_compare(num_pick,num_guess)
+	while record_list_round[0] != record_list_round[count]:
+		range_change(range_choose,num_guess)
+		num_guess = number_guess(range_choose)
+		num_guess.review
+
+
 
 
 # create frame
-
-# create a record_list
-
+frame = simplegui.create_frame("guess_number",400,400)
+frame.add_button('start', game_start, 100)
 
 
 #start game
-
+frame.start()
